19adc0ecd5fbb18d5a6ffe000bd367f622f47eec
Tara :3 <samikshatara@gmail.com>
2025-02-04 22:02:23 +0530

    additions to the pq

--- src/backend/datastructures/priorityqueue.py
+++ src/backend/datastructures/priorityqueue.py
@@ -6,3 +6,18 @@ class Task:                             # Tasks with a set priority
         self.task = task
         self.priority = priority
 
+    def __lt__(self, other):
+        return self.priority < other.priority
+    
+    def __repr__(self):
+         return f"Task(priority={self.priority}, task={self.task})
+
+class PriorityQueue:
+    def __init__(self, task=None):
+        self.queue = []
+        self.lock = threading.Lock()
+
+        if task:
+            self.queue.append(task)
+
+    
