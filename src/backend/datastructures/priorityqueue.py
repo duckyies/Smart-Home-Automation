23d9328e4e29bed1974ba2f5d36bc804b70ac188
Tara :3 <samikshatara@gmail.com>
2025-02-04 22:04:03 +0530

    pq

--- src/backend/datastructures/priorityqueue.py
+++ src/backend/datastructures/priorityqueue.py
@@ -1,6 +1,8 @@
 import threading
 class EmptyListAccessException(Exception):
     pass
+
+
 class Task:                             # Tasks with a set priority
     def __init__(self, task, priority):
         self.task = task
@@ -11,6 +13,8 @@ class Task:                             # Tasks with a set priority
     
     def __repr__(self):
          return f"Task(priority={self.priority}, task={self.task})
+
+         
 
 class PriorityQueue:
     def __init__(self, task=None):
