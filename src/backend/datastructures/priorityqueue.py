8daac0d1acb33493bbe2f1aaf989562e619845de
Tara :3 <samikshatara@gmail.com>
2025-02-04 22:13:15 +0530

    pq

--- src/backend/datastructures/priorityqueue.py
+++ src/backend/datastructures/priorityqueue.py
@@ -12,9 +12,9 @@ class Task:                             # Tasks with a set priority
         return self.priority < other.priority
     
     def __repr__(self):
-         return f"Task(priority={self.priority}, task={self.task})
+         return f"Task(priority={self.priority}, task={self.task})"
 
-         
+
 
 class PriorityQueue:
     def __init__(self, task=None):
@@ -23,5 +23,21 @@ class PriorityQueue:
 
         if task:
             self.queue.append(task)
+
+            
+    def enqueue(self, new_task):
+        with self.lock:
+            if not self.queue:
+                self.queue.append(new_task)
+                return
+            
+            # Inserting the task in sorted order
+            for i, task in enumerate(self.queue):
+                if task.priority > new_task.priority:
+                    self.queue.insert(i, new_task)
+                    return
+
+            # If not inserted earlier, add at the end
+            self.queue.append(new_task)
 
     
